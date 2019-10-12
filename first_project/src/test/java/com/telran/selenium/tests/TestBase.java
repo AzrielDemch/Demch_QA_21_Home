2fa1f22b6cce1474a0504d3571ad76e1e439f5ae
AzrielDemch <54112283+AzrielDemch@users.noreply.github.com>
2019-10-12 19:20:52 +0300

    To add logger to Wikipedia project

--- first_project/src/test/java/com/telran/selenium/tests/TestBase.java
+++ first_project/src/test/java/com/telran/selenium/tests/TestBase.java
@@ -2,8 +2,15 @@ package com.telran.selenium.tests;
 
 import com.telran.selenium.manager.ApplicationManager;
 import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 public class TestBase {
 
@@ -19,5 +26,17 @@ public class TestBase {
         app.stop();
     }
 
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
 
+
+    @BeforeMethod
+    public void startLogger(Method m, Object p){
+        logger.info("Start test - " + m.getName());
+        System.out.println("____________________");
+    }
+
+    @AfterMethod
+    public void endLogger(Method m){
+            logger.info("End test - " + m.getName());
+        }
 }
--- first_project/src/test/java/com/telran/selenium/tests/TestSearchCity.java
+++ first_project/src/test/java/com/telran/selenium/tests/TestSearchCity.java
@@ -1,10 +1,14 @@
 package com.telran.selenium.tests;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestSearchCity extends TestBase{
 
-@Test
+    @Test
     public void searchCity(){
 
     app.getCityHelper().typeNameOfCityInField("Kiev");
